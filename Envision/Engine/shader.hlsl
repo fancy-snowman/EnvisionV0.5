c5811e09812d473b7e6956430bbee990aebf7419
fancy-snowman <mackan.holmberg@gmail.com>
2022-07-28 19:45:28 +0200

    Cleaned up code in shader.

--- Envision/Engine/shader.hlsl
+++ Envision/Engine/shader.hlsl
@@ -163,32 +163,19 @@ VS_OUT VS_main(VS_IN input, uint instanceIndex : SV_InstanceID)
 
 float4 PS_main(VS_OUT input) : SV_TARGET
 {
+	// Retrieve indexed data structures
 	InstanceData instance = InstanceBuffers[input.InstanceIndex];
 	MaterialData material = MaterialBuffers[instance.MaterialIndex];
 
-
-	//float factor = dot(input.Normal, dirToSun);
-	//factor = clamp(factor, 0.3f, 1.0f) * 2.0f;
-
-	//float3 color = material.DiffuseFactor;
-	//color *= factor;
-
-	//return float4(material.MaterialID / 10.f, 0.0f, 0.0f, 1.0f);
-	//return float4(color, 1.0f);
-
 	// Illumination vectors
-	//float3 L_dirToLight = -1.f * normalize(float3(-1.0f, -2.f, 0.8f));
 	float3 L_dirToLight = normalize(float3(1.0f, 2.f, 0.8f));
-	//float3 L_dirToLight = normalize(float3(1.0f, 0.001f, 0.0f));
-	//float3 R_dirLightReflection = normalize(2.f * dot(L_dirToLight, input.Normal) * input.Normal - L_dirToLight);
 	float3 R_dirLightReflection = -1.0f * normalize(reflect(L_dirToLight, input.Normal));
 	float3 V_dirToEye = normalize(Camera.Position - input.WorldPosition);
 
 	float3 ambient = DiffuseTextures[material.AmbientMapIndex].Sample(Sampler, float2(input.Texcoord)).xyz;
 	float3 diffuse = DiffuseTextures[material.DiffuseMapIndex].Sample(Sampler, float2(input.Texcoord)).xyz;
 	float3 specular = DiffuseTextures[material.SpecularMapIndex].Sample(Sampler, float2(input.Texcoord)).xyz;
-	//float3 ambient = float3(1.0f, 1.0f, 1.0f);
-	//float3 specular = float3(1.0f, 1.0f, 1.0f);
+
 	ambient = ambient * float4(material.AmbientFactor, 1.0f);
 	diffuse = diffuse * float4(material.DiffuseFactor, 1.0f);
 	specular = specular * float4(material.SpecularFactor, 1.0f);
@@ -227,34 +214,6 @@ float4 PS_main(VS_OUT input) : SV_TARGET
 		}
 	}
 
-	//if (material.Shininess < 0.f)
-	//	return float4(1.0f, 0.0f, 0.0f, 1.0f);
-	//else if (material.Shininess == 0.0f)
-	//	return float4(0.0f, 0.0f, 1.0f, 1.0f);
-	//else
-	//	return float4(0.0f, 1.0f, 0.0f, 1.0f);
-
-	//float3 test = specular;
-	//if (test.x < 0.0f || test.y < 0.0f || test.z < 0.0f)
-	//	return float4(1.0f, 0.0f, 0.0f, 1.0f);
-	//else
-	//	return float4(0.0f, 1.0f, 0.0f, 1.0f);
-
-	//if (dot(V_dirToEye, input.Normal) <= 0.0f)
-	//	specular = float3(0.f, 0.f, 0.f);
-
 	float3 illumination = ambient + diffuse * 2.0f + specular * 2.0f;
-
-	//float dist = length(input.WorldPosition - Camera.Position) / 10000.f;
-	//return float4(dist, dist, dist, 1.0f);
-
-	//return float4(material.DiffuseFactor, 1.0f);
 	return float4(illumination, 1.0f);
-
-	//return float4(input.Texcoord, 0.0f, 1.0f);
-
-	//float4 color = float4(0.8f, 0.6f, 0.5f, 1.0f);
-	//color = color * factor;
-	//color.w = 1.0f;
-	//return color;
 }
